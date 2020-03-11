11977f0fdf423935fad3a54395c7a159c74d1778
Toni Oriol <tonioriol@gmail.com>
2020-03-11 10:24:05 +0100

    Fixing a Poor Judgement Call

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -4,9 +4,6 @@ defmodule Hangman do
 
   defdelegate new, to: Game
   defdelegate tally(game), to: Game
+  defdelegate make_move(game, guess), to: Game
 
-  def make_move(game, guess) do
-    game = Game.make_move(game, guess)
-    {game, tally(game)}
-  end
 end
