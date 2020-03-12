e27d4c4fd7b480705c12f4b37ef148588fa23ba7
Toni Oriol <tonioriol@gmail.com>
2020-03-12 17:00:55 +0100

    Installing phoenix

--- text_client/lib/text_client/interact.ex
+++ text_client/lib/text_client/interact.ex
@@ -1,9 +1,11 @@
 defmodule TextClient.Interact do
 
+  @hangman_server :hangman@plasma
+
   alias TextClient.{Player, State}
 
   def start() do
-    Hangman.new()
+    new_game()
     |> setup_state()
     |> Player.play()
   end
@@ -13,5 +15,9 @@ defmodule TextClient.Interact do
       game_service: game,
       tally: Hangman.tally(game)
     }
+  end
+
+  defp new_game() do
+    :rpc.call(@hangman_server, Hangman, :new, [])
   end
 end
